0d9e2d1df37c68cf8d18a8066ae9c4fb6a64d9e3
thomas_girard45 <54374885+dalton-x@users.noreply.github.com>
2024-06-30 11:16:41 +0200

    suppression de google map + mise en place des coordonnées

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,36 +1,15 @@
 $(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-
-  initMap();
+  $('.gallery').mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3
+    },
+    lightBox: true,
+    lightboxId: 'myAwesomeLightbox',
+    showTags: true,
+    tagsPosition: 'top'
+  });
 });
-
-function initMap() {
-    // Coordonnées de votre adresse
-    const location = { lat: 44.8465743, lng: -0.6065365 };
-  
-    // Créer la carte
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12,
-      center: location,
-    });
-  
-    // Marker sur l'adresse
-    new google.maps.Marker({
-      position: location,
-      map: map,
-      title: "68 avenue Alsace-Lorraine, 33200 Bordeaux",
-    });
-  }
-  
